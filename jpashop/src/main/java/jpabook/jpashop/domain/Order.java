d54d8c089fbbdc17fe4abbc7390786d17c8be151
Jeewoo <cheltus11@gmail.com>
2022-06-05 19:29:28 +0900

    [Order] XToMany fetch = LAZY 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -30,7 +30,7 @@ public class Order {
 	 @Column(name = "order_id")
 	 private Long id;
 	
-	@ManyToOne 
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
 	
@@ -46,7 +46,20 @@ public class Order {
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;//주문상태 [ORDER, CANCEL]
 	
-	
+	//==연관관계 메서드==//
+	 public void setMember(Member member) {
+		 this.member = member;
+		 member.getOrders().add(this);
+	 }
 
-	
+	 public void addOrderItem(OrderItem orderItem) {
+		 orderItems.add(orderItem);
+		 orderItem.setOrder(this);
+	 }
+	 
+	 public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	 }
+	 
 }
